a464cbaa9c791426d358f13421f1de907b169763
AbdullahAlAnasSaimon <abdullahalanassaimon.swd@gmail.com>
2022-12-29 20:49:23 +0600

    added vercel json file

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -15,12 +15,26 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 async function run(){
   const postCollection = client.db(process.env.BITIT_MONGO_USER_DB).collection('posts');
+  const commentCollection = client.db(process.env.BITIT_MONGO_USER_DB).collection('comments');
 
   app.post('/posts', async(req, res) =>{
     const postData = req.body;
     const result = await postCollection.insertOne(postData);
     res.send(result);
   })
+
+  app.get('/posts', async(req, res) =>{
+    const query = {};
+    const result = await postCollection.find(query).sort({_id: - 1}).toArray();
+    res.send(result);
+  })
+
+  app.post('/comment', async(req, res) =>{
+    const comment = req.body;
+    const result = await commentCollection.insertOne(comment);
+    res.send(result);
+  })
+
 }
 run().catch(err => console.error(err));
 
